497704ee9308a3e9a6b51c8973975d7adb1b5e7d
zgulen <gulenzubeyir@gmail.com>
2022-06-29 21:57:09 +0300

    done

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -2,13 +2,25 @@ import { useState } from "react";
 import "./item.css"
 
 const Item = ({ card }) => {
+    const [showCard, setShowCard] = useState(true)
     const {name, img,options} = card
     console.log(img);
     
   return (
-    <div className="card">
+    <div className="card" onClick={() => setShowCard(!showCard)}>
+        {
+            showCard ? <div>
         <img className="cards-logo" src={img} alt= {name} />
         <h3 className="cards-title">{name}</h3>
+        </div>: <ul>
+            {options.map((e,i)=>{
+                    return(
+                        <li key={i}>{e}</li>
+                    )
+                })
+            }
+        </ul>
+        }
     </div>
   )
 }
