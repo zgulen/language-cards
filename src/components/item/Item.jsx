040760798b508e5fb4f79c3937808cf943c46fdf
zgulen <gulenzubeyir@gmail.com>
2022-06-30 11:40:59 +0300

    spinner

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,25 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./item.css"
 
 const Item = ({ card }) => {
     const [showCard, setShowCard] = useState(true)
     const {name, img,options} = card
     console.log(img);
+    useEffect(() => {
+        let spinner
+        if (showCard === false){
+            spinner = setTimeout(() => {
+                    setShowCard(!showCard)
+                }, 5000);
+            }
+            return () => clearTimeout(spinner);
+        
+    },[showCard])
+
+    const handleCard = () => {
+        setShowCard(!showCard)
+    }
     
   return (
-    <div className="card" onClick={() => setShowCard(!showCard)}>
+    <div className="card" onClick={handleCard}>
         {
-            showCard ? <div>
+            showCard ? (<div>
         <img className="cards-logo" src={img} alt= {name} />
         <h3 className="cards-title">{name}</h3>
-        </div>: <ul>
+        </div> ): (<ul>
             {options.map((e,i)=>{
                     return(
                         <li key={i}>{e}</li>
                     )
                 })
             }
-        </ul>
+        </ul>)
         }
     </div>
   )
